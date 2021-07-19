53245a8e07f65064f88194878a441655c02e1227
Ihar <galuropek@gmail.com>
2021-07-19 20:29:38 +0300

    task_1: area of a triangle

---
+++ task_1/area_of_a_triangle.rb
@@ -0,0 +1,7 @@
+print "Введите основание треугольника: "
+base_of_triangle = gets.chomp.to_f
+print "Введите высоту треугольника: "
+height_of_triangle = gets.chomp.to_f
+
+area_of_triangle = 1.0 / 2 * base_of_triangle * height_of_triangle
+puts "Площадь треульника: #{area_of_triangle}"
