45c6e47631af105ab9df3c479a18b90d2827c485
Ihar <galuropek@gmail.com>
2021-07-26 10:23:32 +0300

    add station class

---
+++ task_3/station.rb
@@ -0,0 +1,20 @@
+class Station
+  attr_reader :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def add_train(train)
+    @trains << train
+  end
+
+  def train_departure
+    @trains.shift
+  end
+
+  def show_trains_by_type(type)
+    trains.select { |train| train.type == type }
+  end
+end
