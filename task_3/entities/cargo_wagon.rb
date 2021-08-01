1e379d0b744d12e74b6744ba70e996d982610912
Ihar <galuropek@gmail.com>
2021-08-01 23:51:16 +0300

    add overall_volume to caro_wagon

--- task_3/entities/cargo_wagon.rb
+++ task_3/entities/cargo_wagon.rb
@@ -1,4 +1,24 @@
 require_relative 'wagon'
 
 class CargoWagon < Wagon
+
+  attr_accessor :overall_volume
+
+  def initialize(type, overall_volume)
+    super(type)
+    @overall_volume = overall_volume
+    @occupied_volume = 0.0
+  end
+
+  def add_cargo(volume)
+    @occupied_volume += volume if (@occupied_volume + volume) < overall_volume
+  end
+
+  def occupied_volume
+    @occupied_volume
+  end
+
+  def free_volume
+    overall_volume - @occupied_volume
+  end
 end
