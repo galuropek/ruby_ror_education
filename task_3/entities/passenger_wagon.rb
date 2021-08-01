7145783769ebbc0a7dfceea76597c546deaf81ec
Ihar <galuropek@gmail.com>
2021-08-01 23:51:43 +0300

    add number_of_seats to PassengerWagon

--- task_3/entities/passenger_wagon.rb
+++ task_3/entities/passenger_wagon.rb
@@ -1,4 +1,24 @@
 require_relative 'wagon'
 
 class PassengerWagon < Wagon
+
+  attr_accessor :number_of_seats
+
+  def initialize(type, number_of_seats)
+    super(type)
+    @number_of_seats = number_of_seats
+    @occupied_seats = 0
+  end
+
+  def take_seat
+    @occupied_seats += 1 if @occupied_seats < number_of_seats
+  end
+
+  def occupied_seats
+    @occupied_seats
+  end
+
+  def free_seats
+    number_of_seats - @occupied_seats
+  end
 end
